06ad24773e377a7f7360bf73d95b4574788e6023
annam015 <anamariadinu15@yahoo.com>
2024-04-14 13:07:56 +0300

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Cinemate.Models;
+using Cinemate.Views;
+using Microsoft.Extensions.Logging;
+
+namespace Cinemate
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Teko-Bold.ttf", "TekoBold");
+                    fonts.AddFont("Teko-Light.ttf", "TekoLight");
+                    fonts.AddFont("Teko-Medium.ttf", "TekoMedium");
+                    fonts.AddFont("Teko-Regular.ttf", "TekoRegular");
+                    fonts.AddFont("Teko-SemiBold.ttf", "TekoSemiBold");
+
+                });
+
+            builder.Services.AddSingleton<ImageCollection>();
+            builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton<IntroPage>();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
